e3a06916ebc3de8539a386aca95160f9211d2853
Andrey Nascimento <candreynascimento@gmail.com>
2025-01-18 16:48:07 -0300

    feat: create course model

---
+++ src/Domain/Entities/Course.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Domain\Entities;
+
+class Course
+{
+    private $id;
+    private $name;
+    private $description;
+    private $status;
+    private $image;
+    private $created_at;
+    private $updated_at;
+
+    public function __construct($id, $name, $description, $status, $image, $created_at, $updated_at)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->description = $description;
+        $this->status = $status;
+        $this->image = $image;
+        $this->created_at = $created_at;
+        $this->updated_at = $updated_at;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    public function getUpdatedAt()
+    {
+        return $this->updated_at;
+    }
+}
