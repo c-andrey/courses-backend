c39d8fbff46572a8386fb6aa865cb39ddce8b1fe
Andrey Nascimento <candreynascimento@gmail.com>
2025-01-18 15:31:19 -0300

    feat: database singleton

---
+++ src/Infrastructure/Database/DatabaseConnection.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Infraestructure\Database;
+
+use PDO;
+use PDOException;
+
+class DatabaseConnection
+{
+    private static ?PDO $connection = null;
+
+    public static function getConnection(): PDO
+    {
+        if (self::$connection === null) {
+            try {
+                self::$connection = new PDO('mysql:host=localhost;dbname=desafio_revvo;charset=utf8', 'root', 'abc123');
+                self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                die('Erro na conexão com o banco de dados: ' . $e->getMessage());
+            }
+        }
+
+        return self::$connection;
+    }
+}
